afda3bbebc09eaa3000feb9d46f8c140c322b6bc
8headswillroll8 <lejohansen91@gmail.com>
2026-06-14 18:18:24 +0200

    fix: include all pages in Vite build

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,17 @@
 import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
+import { resolve } from "path";
 
 export default defineConfig({
   base: "/framework-assignment/",
   plugins: [tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+        feed: resolve(__dirname, "feed/index.html"),
+        profile: resolve(__dirname, "profile/index.html"),
+      },
+    },
+  },
 });
